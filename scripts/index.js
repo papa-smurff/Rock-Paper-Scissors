868fe9ebdf1a0328ba195867e39cbdf82823f740
papa-smurff <122695489+papa-smurff@users.noreply.github.com>
2023-02-09 17:08:13 +0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,6 @@ const optionElements = [
 
 document.body.addEventListener('click', showDropdown)
 btnConfirm.addEventListener('click', confirmFunction)
-btnConfirm.addEventListener('click', round)
 
 listItems.forEach((el, index) => {
     el.innerHTML = `<span>${optionElements[index]['name']}</span> <i class="fa-regular ${optionElements[index]['iconClass']}"></i>`
@@ -16,23 +15,25 @@ listItems.forEach((el, index) => {
     })
 })
 
+function randomizer() { return Math.floor(Math.random() * 3) }
+
 function showDropdown(el) {
-    if(el.target.tagName !== 'INPUT') {
-        dropdown.classList.remove('active')
-    } else {
-        dropdown.classList.toggle('active')
-    }
+    (el.target.tagName !== 'INPUT')
+        ? dropdown.classList.remove('active')
+        : dropdown.classList.toggle('active')
 }
 
 function confirmFunction() {
-    let randomValue = randomizer()
-    let player = optionElements.filter(el => textBox.dataset.id === el.id)
-    let random = optionElements[randomValue]
-    playerСhoiceImg.src = player[0]['imgSrc']
-    randomSelectionImg.src = random['imgSrc']
-    totalAmount(player[0], random)
+    let player = (textBox.dataset.id) ? optionElements.filter(el => textBox.dataset.id === el.id) : null
+    if (player) {
+        let randomValue = randomizer()
+        let random = optionElements[randomValue]
+        playerСhoiceImg.src = player[0]['imgSrc']
+        randomSelectionImg.src = random['imgSrc']
+        totalAmount(player[0], random)
+        round()
+    }
 }
-
 
 let plrCount = 0
 let rdmCount = 0
@@ -40,20 +41,15 @@ function totalAmount(player, random) {
     const plrRock = player['name'] === 'rock'
     const plrPaper = player['name'] === 'paper'
     const plrScissors = player['name'] === 'scissors'
-
     const rdmRock = random['name'] === 'rock'
     const rdmPaper = random['name'] === 'paper'
     const rdmScissors = random['name'] === 'scissors'
-
-
     if (plrRock && rdmScissors || plrPaper && rdmRock || plrScissors && rdmPaper) {
         playerСount.textContent = ++plrCount
-        if (plrCount >= 3) resetValues('Win', `${plrCount}:${rdmCount}`)
+        if (plrCount >= 3) overplayFunc('Win', `${plrCount}:${rdmCount}`)
     } else if (rdmRock && plrScissors || rdmPaper && plrRock || rdmScissors && plrPaper) {
         randomCount.textContent = ++rdmCount
-        if (rdmCount >= 3) resetValues('Loss', `${plrCount}:${rdmCount}`)
-    } else {
-        return null
+        if (rdmCount >= 3) overplayFunc('Loss', `${plrCount}:${rdmCount}`)
     }
 }
 
@@ -65,16 +61,17 @@ function round() {
     }, 2000)
 }
 
-function resetValues(result, total) {
-    reGame.classList.toggle('showResult')
-    reGame.innerHTML = `<h2>${result}</h2><h3>${total}</h3><button class="reset">ReGame</button>`
-    document.querySelector('.regame>.reset').addEventListener('click', () => {
-        textBox.value = null
-        playerСount.textContent = plrCount = 0
-        randomCount.textContent = rdmCount =0
-        playerСhoiceImg.src = randomSelectionImg.src = './images/mystery-box.png'
-        reGame.classList.toggle('showResult')
-    })
+function overplayFunc(result, total) {
+    setTimeout(() => {
+        overPlay.classList.toggle('showResult')
+        overPlay.innerHTML = `<h2>${result}</h2><h3>${total}</h3><button>Overplay</button>`
+        document.querySelector('.overplay>button').addEventListener('click', () => {
+            textBox.value = null
+            textBox.toggleAttribute('data-id')
+            playerСount.textContent = plrCount = 0
+            randomCount.textContent = rdmCount = 0
+            playerСhoiceImg.src = randomSelectionImg.src = './images/mystery-box.png'
+            overPlay.classList.toggle('showResult')
+        })
+    }, 1800)
 }
-
-function randomizer() { return Math.floor(Math.random() * 3) }
